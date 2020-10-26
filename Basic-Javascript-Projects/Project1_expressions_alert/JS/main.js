b0a37e229a2527264e98bdf2df55d6ec67ea2280
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2020-10-26 15:26:33 -0600

    Update main.js

--- Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,21 @@
-alert('Hello World!')
-window.alert('Greetings and salutations!')
-document.write('Hello there!')
+alert('Hello World!');
+window.alert('Greetings and salutations!');
+document.write('Hello there!');
+
+var a = 'This is a string!';
+document.write(a);
+
+const name = 'Jason';
+
+let greetName = 'Hello ' + name;
+
+document.write(greetName + "<br><br>");
+
+
+document.write("Obi Wan said \"Hello there!\"<br> \"General Kenobi!\" Grevious responded.<br><br>");
+
+document.write("I, Giorno Giovana, " + " have a dream!<br><br>");
+
+let dioStandcommand = "Za Worldo! " + "Stop Time!"
+
+document.write(dioStandcommand)
