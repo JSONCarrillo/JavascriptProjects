58f0283792bad3dc5cc72752d00a6754d74e7941
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2020-10-26 15:58:56 -0600

    add comments to main.js

--- Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -2,29 +2,31 @@ alert('Hello World!');
 window.alert('Greetings and salutations!');
 document.write('Hello there!');
 
-var a = 'This is a string!';
-var a = a.fontcolor("red")
+var a = 'This is a string!'; //claims 'a' string
+var a = a.fontcolor("red") //makes 'a' string red
 document.write(a);
 
 var name = 'Jason';
 
-var greetName = 'Hello ' + name;
-var greetName = greetName.fontcolor("blue")
+var greetName = 'Hello ' + name; //consentrates greetName string
+var greetName = greetName.fontcolor("blue") // makes greetName string blue
 
 
 document.write(greetName + "<br><br>");
 
 
-document.write("Obi Wan said \"Hello there!\"<br> \"General Kenobi!\" Grevious responded.<br><br>");
+document.write("Obi Wan said \"Hello there!\"<br> \"General Kenobi!\" Grevious responded.<br><br>"); // string with '\' breaks
 
 document.write("I, Giorno Giovana, " + " have a dream!<br><br>");
 
-var dioStandcommand = "Za Worldo! " + "Stop Time!"
-var dioStandcommand = dioStandcommand.fontcolor("orange")
+var dioStandcommand = "Za Worldo! " + "Stop Time!" // claims dioStandcommand
+var dioStandcommand = dioStandcommand.fontcolor("orange") // makes dioStandCommand orange
 
 document.write(dioStandcommand);
 
-var joestars = greatGreatgrandfather = "Jonathan", grandFather = "George", Father = "Joseph", son = "Josuke", grandson = "Jotaro"
-var son = son.fontcolor("green")
+var joestars = greatGreatgrandfather = "Jonathan", grandFather = "George", Father = "Joseph", son = "Josuke", grandson = "Jotaro" //claims joestars variable(s)
+var son = son.fontcolor("green") //makes 'son' variable green
 
-document.write(son);
+document.write(son + "<br><br>");
+
+document.write(6 * 24) // writes '6 x 24' into the HTML document
