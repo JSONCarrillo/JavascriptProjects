62feffe5aa71d4a9ebe869dc16f077ab36a0218f
JSONCarrillo <72313396+JSONCarrillo@users.noreply.github.com>
2020-10-28 10:57:11 -0600

    create project2_functions folder

---
+++ Basic-Javascript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+function addNumber() {
+    var num1 = 12;
+    var num2 = 48;
+    var answer = num1 + num2;
+    document.getElementById('addAnswer').innerHTML = answer;
+}
+
+function concatenate() {
+    var sentence = 'Hello there! ';
+    sentence += 'General Kenobi!';
+    document.getElementById('concatenate').innerHTML = sentence;
+}
+
